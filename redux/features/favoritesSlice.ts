d1b8efada4113a528734e950aee53a5969d7989e
sai krishna <saikrishnabanda55@gmail.com>
2025-03-20 13:24:36 +0530

    Update redux store

--- redux/features/favoritesSlice.ts
+++ redux/features/favoritesSlice.ts
@@ -1,4 +1,4 @@
- import getLastValueBeforeSlash from "@/utils/getLastValueBeforeSlash";
+import getLastValueBeforeSlash from "@/utils/getLastValueBeforeSlash";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface Character {
@@ -38,8 +38,21 @@ const favoritesSlice = createSlice({
         (char: any) => getLastValueBeforeSlash(char.url) !== action.payload
       );
     },
+    editFavorite: (
+      state,
+      action: PayloadAction<{ id: string; gender: string; height: string }>
+    ) => {
+      const character = state.favorites.find(
+        (fav: any) => getLastValueBeforeSlash(fav.url) === action.payload.id
+      );
+      if (character) {
+        character.gender = action.payload.gender;
+        character.height = action.payload.height;
+      }
+    },
   },
 });
 
-export const { addFavorite, removeFavorite } = favoritesSlice.actions;
+export const { addFavorite, removeFavorite, editFavorite } =
+  favoritesSlice.actions;
 export default favoritesSlice.reducer;
